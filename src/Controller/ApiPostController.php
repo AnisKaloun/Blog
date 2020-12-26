b3d703f96624bd3b413018e874afb4b80180a2c9
Kaloun Anis <kaloun.anis@gmail.com>
2020-12-26 23:39:39 +0100

    API rest Test

--- src/Controller/ApiPostController.php
+++ src/Controller/ApiPostController.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Contracts\HttpClient\ResponseInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
 class ApiPostController extends AbstractController
 {
     /**
